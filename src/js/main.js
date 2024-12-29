a0b3fc132d99b712961ab142193110888254f0ae
v-lavs <lavskavanda@gmail.com>
2024-12-29 19:22:17 +0200

    added js btn_up

--- src/js/main.js
+++ src/js/main.js
@@ -464,6 +464,35 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+//BTN-UP
+    (function () {
+
+        function trackScroll() {
+            let scrolled = window.pageYOffset;
+            let coords = document.documentElement.clientHeight;
+
+            if (scrolled > coords) {
+                goTopBtn.classList.add('show');
+            }
+            if (scrolled < coords) {
+                goTopBtn.classList.remove('show');
+            }
+        }
+
+        function backToTop() {
+            if (window.pageYOffset > 0) {
+                window.scrollBy(0, -80);
+                setTimeout(backToTop, 0);
+            }
+        }
+
+        const goTopBtn = document.querySelector('.btn_up');
+
+        window.addEventListener('scroll', trackScroll);
+        goTopBtn.addEventListener('click', backToTop);
+    })();
+
+
 });
 
 
